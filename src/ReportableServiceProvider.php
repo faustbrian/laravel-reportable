db2e88fe83f2d7b2102b70b409b87eb241967c0f
Brian Faust <hello@brianfaust.de>
2016-12-03 19:33:02 +0200

    :hammer: Start making use of some PHP 7.1 goodies

--- src/ReportableServiceProvider.php
+++ src/ReportableServiceProvider.php
@@ -9,18 +9,20 @@
  * file that was distributed with this source code.
  */
 
+declare(strict_types=1);
+
 namespace BrianFaust\Reportable;
 
 use BrianFaust\ServiceProvider\ServiceProvider;
 
 class ReportableServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(): void
     {
         $this->publishMigrations();
     }
 
-    public function getPackageName()
+    public function getPackageName(): string
     {
         return 'reportable';
     }
