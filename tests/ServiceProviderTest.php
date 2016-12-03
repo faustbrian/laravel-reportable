56bb33ed13397b8f498a835c4567d030b19dac35
Brian Faust <hello@brianfaust.de>
2016-12-03 17:33:09 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -11,6 +11,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Laravel Reportable.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Tests\Reportable;
 
 use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
