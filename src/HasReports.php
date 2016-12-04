4c9571c9010ea41a9d3cdb30fb2e5c9f96ba3173
Brian Faust <hello@brianfaust.de>
2016-12-04 09:06:23 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/HasReports.php
+++ src/HasReports.php
@@ -23,10 +23,6 @@ declare(strict_types=1);
 namespace BrianFaust\Reportable;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait HasReports
